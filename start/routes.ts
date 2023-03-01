01eb9f3404006f1f2e295ba79a5bf72fdfceb378
DevRobertoC <rc.rodriguez@live.com>
2023-03-01 14:21:43 -0500

    Proyecto Auth jwt

---
+++ start/routes.ts
@@ -0,0 +1,45 @@
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| This file is dedicated for defining HTTP routes. A single file is enough
+| for majority of projects, however you can define routes in different
+| files and just make sure to import them inside this file. For example
+|
+| Define routes in following two files
+| ├── start/routes/cart.ts
+| ├── start/routes/customer.ts
+|
+| and then import them inside `start/routes.ts` as follows
+|
+| import './routes/cart'
+| import './routes/customer'
+|
+*/
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async () => {
+  return { hello: 'world' }
+}).middleware("auth")
+
+Route.group(() =>{
+  Route.post("/register", "UsuariosController.registrar");
+  Route.post("/login", "UsuariosController.login");  
+}).prefix("api")
+
+Route.group(() => {
+  Route.post("/register", "PerfilsController.registrar");
+  Route.get("/listar", "PerfilsController.getListarPerfiles");
+}).prefix("/api/perfil").middleware("auth")
+
+Route.group(() => {
+  Route.post("/register", "LibrosController.registrar");
+  Route.get("/listar", "LibrosController.getListarLibros");
+}).prefix("/api/libro").middleware("auth")
+
+Route.group(() => {
+  Route.post("/register", "TipoDocumentosController.registrar");
+  Route.get("/listar", "TipoDocumentosController.getListarDocumentos");
+}).prefix("/api/documentos")
